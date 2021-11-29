41d5923f898d6bfd692f47833e5ad1726501bf3f
Carlos A Hernández C <xcarlosdesarrolladorx@gmail.com>
2021-11-28 19:08:15 -0500

    Api de produccion

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 // PRODUCTION
-// const URL_SERVER = "https://kingamer-api.herokuapp.com";
+const URL_SERVER = "https://kingamer-api.herokuapp.com";
 
 // DEVELOP
-const URL_SERVER = 'http://localhost:3001'
+// const URL_SERVER = 'http://localhost:3001'
 const URL_API = `${URL_SERVER}/api`
 
 const URL_IMAGE = `${URL_API}/load/image/`
